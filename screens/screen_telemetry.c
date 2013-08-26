689a29723f218c91c62203269e801c4cdab4b6cc
Oliver Dippel <oliver@multixmdia.org>
2013-08-26 18:25:57 +0200

    other telemetry display

--- screens/screen_telemetry.c
+++ screens/screen_telemetry.c
@@ -31,7 +31,67 @@ void draw_value_display (ESContext *esContext, float x1, float y1, float fsize, 
 	}
 }
 
+void draw_value_bar (ESContext *esContext, float x1, float y1, float x2, float y2, uint8_t type, float min, float max, float warning, float critical, float value) {
+	char tmp_str[1024];
+//	draw_box_f(esContext, x1, y1, x2, y2, 100, 100, 100, 125);
+
+	float x_diff = x2 - x1;
+	float y_diff = y2 - y1;
+	float v_diff = max - min;
+
+	float y_warning = y2 - (y_diff / v_diff * (warning - min));
+	float y_critical = y2 - (y_diff / v_diff * (critical - min));
+	float y_max = y2 - (y_diff / v_diff * (max - min));
+	float y_value = y2 - (y_diff / v_diff * (value - min));
+
+	draw_box_f(esContext, x1 + x_diff / 6.0 * 3.0, y_max, x1 + x_diff / 6.0 * 4.0, y_warning, 0, 255, 0, 255);
+	draw_box_f(esContext, x1 + x_diff / 6.0 * 3.0, y_warning, x1 + x_diff / 6.0 * 4.0, y_critical, 255, 255, 0, 255);
+	draw_box_f(esContext, x1 + x_diff / 6.0 * 3.0, y_critical, x1 + x_diff / 6.0 * 4.0, y2, 255, 0, 0, 255);
+
+	draw_line_f(esContext, x1 + x_diff / 6.0 * 3.0, y_warning, x1 + x_diff / 6.0 * 4.0 + x_diff / 10.0, y_warning, 255, 255, 0, 255);
+	sprintf(tmp_str, "%0.1f", warning);
+	draw_text_f(esContext, x1 + x_diff / 6.0 * 4.0 + x_diff / 10.0, y_warning - 0.02, 0.04, 0.04, FONT_PINK, tmp_str);
+
+	draw_line_f(esContext, x1 + x_diff / 6.0 * 3.0, y_critical, x1 + x_diff / 6.0 * 4.0 + x_diff / 10.0, y_critical, 255, 0, 0, 255);
+	sprintf(tmp_str, "%0.1f", critical);
+	draw_text_f(esContext, x1 + x_diff / 6.0 * 4.0 + x_diff / 10.0, y_critical - 0.02, 0.04, 0.04, FONT_PINK, tmp_str);
+
+
+	uint16_t steps = (uint16_t)(y_diff / 0.06);
+	uint16_t step = 0.0;
+	steps = 10;
+	for (step = 0; step <= steps; step++) {
+		draw_line_f(esContext, x1 + x_diff / 6.0 * 3.0, y2 - (y_diff * (float)step / steps), x1 + x_diff / 6.0 * 3.0 - x_diff / 10.0, y2 - (y_diff * (float)step / steps), 255, 255, 255, 255);
+		sprintf(tmp_str, "%0.1f", min + (v_diff * (float)step / steps));
+		draw_text_f(esContext, x1, y2 - (y_diff * (float)step / steps) - 0.02, 0.04, 0.04, FONT_WHITE, tmp_str);
+	}
+
+	steps = 50;
+	for (step = 0; step <= steps; step++) {
+		draw_line_f(esContext, x1 + x_diff / 6.0 * 3.0, y2 - (y_diff * (float)step / steps), x1 + x_diff / 6.0 * 3.0 - x_diff / 20.0, y2 - (y_diff * (float)step / steps), 200, 200, 200, 255);
+	}
+
+	sprintf(tmp_str, "%0.1f", value);
+
+	draw_tria_f(esContext, x1 + x_diff / 6.0 * 3.0, y_value, x1 + x_diff / 6.0 * 4.0 + x_diff / 10.0, y_value - x_diff / 10.0, x1 + x_diff / 6.0 * 4.0 + x_diff / 10.0, y_value + x_diff / 10.0, 255, 255, 255, 255);
+//	draw_text_f(esContext, x1 + x_diff / 6.0 * 4.0 + x_diff / 10.0, y_value - 0.02, 0.04, 0.04, FONT_PINK, tmp_str);
+
+}
+
+
+void draw_value_bar2 (ESContext *esContext, float x1, float y1, float x2, float y2, char *title, char *unit, uint8_t type, float min, float max, float warning, float critical, float value) {
+	char tmp_str[1024];
+	sprintf(tmp_str, title);
+//	draw_text_f(esContext, (x1 + (x2 - x1) / 2) - strlen(tmp_str) * 0.1 / 2.0 * 0.6, y1, 0.1, 0.1, FONT_WHITE, tmp_str);
+	draw_text_f(esContext, x1, y1, 0.1, 0.1, FONT_WHITE, tmp_str);
+	draw_value_bar(esContext, x1, y1 + 0.11, x2, y2 - 0.16, 0, min, max, warning, critical, value);
+	sprintf(tmp_str, "%0.1f%s", value, unit);
+	draw_text_f(esContext, (x1 + (x2 - x1) / 2) - strlen(tmp_str) * 0.1 / 2.0 * 0.6, y2 - 0.13, 0.1, 0.1, FONT_WHITE, tmp_str);
+}
+
+
 void screen_telemetry (ESContext *esContext) {
+	char tmp_str[1024];
 #ifndef SDLGL
 	UserData *userData = esContext->userData;
 #endif
@@ -48,7 +108,7 @@ void screen_telemetry (ESContext *esContext) {
 
 
 	draw_title(esContext, "Telemtry");
-
+/*
 	uint8_t y = 0;
 	draw_value_display(esContext, -1.2, -0.7 + y++ * 0.28, 0.15, 10, 1, "Speed", "%0.1f km/h", ModelData.speed);
 	draw_value_display(esContext, -1.2, -0.7 + y++ * 0.28, 0.15, 10, 1, "Altitude", "%0.1f m", ModelData.p_alt);
@@ -61,9 +121,15 @@ void screen_telemetry (ESContext *esContext) {
 	y++;
 //	draw_value_display(esContext, -1.2, -0.7 + y++ * 0.28, 0.15, 10, 1, "Voltage (TX)", "%0.1f V", ModelData.voltage_tx);
 	draw_value_display(esContext, 0.2, -0.7 + y++ * 0.28, 0.15, 10, 1, "Direction", "%0.1f Grad", ModelData.yaw);
-
+*/
 
 //	draw_button(esContext, "telemetry_test1", VIEW_MODE_TELEMETRY, "TELE", FONT_WHITE, -1.0, 0.9, 0.002, 0.06, 0, 0, telemetry_null, 0);
+
+
+	draw_value_bar2(esContext, -1.2, -0.7, -0.9, 0.0, "SPEED", "km/h", 0, 0.0, 200.0, 0.0, 0.0, ModelData.speed);
+	draw_value_bar2(esContext, -0.7, -0.7, -0.4, 0.0, "ALT", "m", 0, -10.0, 999.0, 0.0, 0.0, ModelData.p_alt);
+	draw_value_bar2(esContext, -0.2, -0.7, 0.1, 0.0, "VOLTS", "V", 0, 0.0, 14.0, 11.0, 10.0, ModelData.voltage_rx);
+
 
 
 #ifdef SDLGL
