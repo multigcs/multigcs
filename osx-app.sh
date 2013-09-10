9e0b97e31fa536a29560d423a625b1cdf0f10834
Oliver Dippel <oliver@multixmdia.org>
2013-09-10 15:32:07 +0200

    adding osx app-package builder

--- osx-app.sh
+++ osx-app.sh
@@ -34,8 +34,39 @@ done
 
 #tiffutil -cathidpicheck multigcs.iconset/icon_32x32.png multigcs.iconset/icon_32x32@2x.png -out multigcs.tiff
 
+
 #hdiutil create -srcfolder "multigcs.app" -volname "multigcs" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW -size 15M multigcs.temp.dmg
 
 device=$(hdiutil attach -readwrite -noverify -noautoopen "multigcs.temp.dmg" | egrep '^/dev/' | sed 1q | awk '{print $1}')
+echo "## $device ##"
+
+#mkdir -p .background
+#cp icon.png .background/
+
+title="multigcs"
+backgroundPictureName="multigcs.png"
+applicationName="multigcs"
+echo '
+   tell application "Finder"
+     tell disk "'${title}'"
+           open
+           set current view of container window to icon view
+           set toolbar visible of container window to false
+           set statusbar visible of container window to false
+           set the bounds of container window to {400, 100, 885, 430}
+           set theViewOptions to the icon view options of container window
+           set arrangement of theViewOptions to not arranged
+           set icon size of theViewOptions to 72
+           set background picture of theViewOptions to file ".background:'${backgroundPictureName}'"
+           make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
+           set position of item "'${applicationName}'" of container window to {100, 100}
+           set position of item "Applications" of container window to {375, 100}
+           update without registering applications
+           delay 5
+           eject
+     end tell
+   end tell
+' | osascript
+
 
 
