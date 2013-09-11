3f12cdb183925e55647cfad198d111c08b24eba7
Oliver Dippel <oliver@multixmdia.org>
2013-09-11 16:51:39 +0200

    adding minizip

--- screens/screen_map.c
+++ screens/screen_map.c
@@ -475,64 +475,52 @@ void get_maps (uint8_t mode) {
 #define UNZIP_ERRWRITE 5
 
 int unzipFile( char* zipfile, char* filename, char* outfile ) {
-  unzFile uf = 0;
-  FILE* fout = 0;
-  int result, bytes;
-  char buffer[1024];
-
-  uf = unzOpen( zipfile );
-  if( !uf )
-  {
-    printf( "Can not open file '%s' !\n", zipfile );
-    return UNZIP_ERROPEN;
-  }
-
-  if( unzLocateFile( uf, filename, 0 ) != UNZ_OK )
-  {
-    printf( "Can not find '%s' inside the zipfile !\n", filename );
-    unzCloseCurrentFile(uf);
-    return UNZIP_ERRNOTFOUND;
-  }
-  
-  if( unzOpenCurrentFile( uf) != UNZ_OK )
-  {
-    printf( "Can not open '%s' inside the zipfile !\n", filename );
-    unzCloseCurrentFile(uf);
-    return UNZIP_ERRNOTFOUND;
-  }
-
-  fout = fopen( outfile,"wb" );
-  if( !fout )
-  {
-    printf( "Can not create file '%s' !\n", outfile );
-    unzCloseCurrentFile(uf);
-    return UNZIP_ERRCREATE;
-  }
-
-  result = UNZIP_OK;
-  for(;;)
-  {
-    bytes = unzReadCurrentFile( uf, buffer, sizeof(buffer) );
-    if( bytes == 0 ) break; /* finish */
-    if( bytes < 0 )
-    {
-      printf( "Error reading %s %d!\n", filename, bytes );
-      result = UNZIP_ERRREAD;
-      break;
-    }
-    else
-      if( fwrite( buffer, 1, bytes, fout )!= bytes )
-      {
-        printf("error in writing extracted file\n");
-        result = UNZIP_ERRWRITE;
-        break;
-      }
-  }
-  
-  fclose( fout );
-  unzCloseCurrentFile( uf );
-
-  return result;
+	unzFile uf = 0;
+	FILE* fout = 0;
+	int result, bytes;
+	char buffer[1024];
+	uf = unzOpen( zipfile );
+	if ( !uf ) {
+		printf( "Can not open file '%s' !\n", zipfile );
+		return UNZIP_ERROPEN;
+	}
+	if ( unzLocateFile( uf, filename, 0 ) != UNZ_OK ) {
+		printf( "Can not find '%s' inside the zipfile !\n", filename );
+		unzCloseCurrentFile(uf);
+		return UNZIP_ERRNOTFOUND;
+	}
+  	if( unzOpenCurrentFile( uf) != UNZ_OK ) {
+		printf( "Can not open '%s' inside the zipfile !\n", filename );
+		unzCloseCurrentFile(uf);
+		return UNZIP_ERRNOTFOUND;
+	}
+	fout = fopen( outfile,"wb" );
+	if( !fout ) {
+		printf( "Can not create file '%s' !\n", outfile );
+		unzCloseCurrentFile(uf);
+		return UNZIP_ERRCREATE;
+	}
+	result = UNZIP_OK;
+	for (;;) {
+		bytes = unzReadCurrentFile( uf, buffer, sizeof(buffer) );
+		if ( bytes == 0 ) {
+			break;
+		}
+		if ( bytes < 0 ) {
+			printf( "Error reading %s %d!\n", filename, bytes );
+			result = UNZIP_ERRREAD;
+			break;
+		} else {
+			if ( fwrite( buffer, 1, bytes, fout )!= bytes ) {
+				printf("error in writing extracted file\n");
+				result = UNZIP_ERRWRITE;
+				break;
+			}
+		}
+	}
+	fclose( fout );
+	unzCloseCurrentFile( uf );
+	return result;
 }
 
 
