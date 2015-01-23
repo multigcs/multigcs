0a54cd809b3262299ad47f4d44f33f72ecbfe3bb
Oliver Dippel <oliver@multixmedia.org>
2015-01-23 11:28:25 +0100

    sitl_joystick: show values on console

--- mavlink/sitl-transmitter/joystick.c
+++ mavlink/sitl-transmitter/joystick.c
@@ -16,47 +16,44 @@
 #include <SDL/SDL.h>
 
 int main ( int argc, char *argv[] ) {
-	int i = 0;
 	int16_t values[8];
 	int sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	struct sockaddr_in gcAddr; 
-
+	int i = 0;
 	if (SDL_InitSubSystem(SDL_INIT_JOYSTICK) < 0) {
 		fprintf(stderr, "Unable to initialize Joystick: %s\n", SDL_GetError());
 		return -1;
 	}
+	fprintf(stderr, "%i joysticks found\n", SDL_NumJoysticks());
 	if (SDL_NumJoysticks() <= 0) {
-		printf("%i joysticks found\n", SDL_NumJoysticks());
+		return -1;
 	}
 	SDL_Joystick* joy1 = SDL_JoystickOpen(0);
 	if (joy1 == NULL) {
-		printf("could not open joystick\n");
+		fprintf(stderr, "could not open joystick\n");
 		return -1;
 	}
 	SDL_JoystickEventState(SDL_QUERY);
-
 	memset(&gcAddr, 0, sizeof(gcAddr));
 	gcAddr.sin_family = AF_INET;
 	gcAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	gcAddr.sin_port = htons(5501);
-
 	for (i = 0; i < 8; ++i) {
 		values[i] = 1500;
 	}
-
+	sendto(sock, values, 16, 0, (struct sockaddr*)&gcAddr, sizeof(struct sockaddr_in));
 	while (1) {
-		SDL_JoystickUpdate ();
+		SDL_JoystickUpdate();
 		for (i = 0; i < SDL_JoystickNumAxes(joy1) && i < 8; ++i) {
 			int16_t value = SDL_JoystickGetAxis(joy1, i);
 			values[i] = value / 66 + 1500;
+			fprintf(stderr, "%i ", values[i]);
 		}
+		fprintf(stderr, "\r");
 		sendto(sock, values, 16, 0, (struct sockaddr*)&gcAddr, sizeof(struct sockaddr_in));
-		SDL_Delay(10);
-
-
+		SDL_Delay(20);
 	}
+	fprintf(stderr, "\n");
 	return 0;
 }
-
-
 
